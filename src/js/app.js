9fda7ea513ff9fac8cc691f02173ba6289fb5f79
Kent C. Dodds <kent@doddsfamily.us>
2016-06-15 23:09:51 -0600

    copied es5 files

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,26 @@
-import 'todomvc-common/base.css'
-import 'todomvc-app-css/index.css'
+/*global app, $on */
+(function () {
+  'use strict';
 
-import View from './view'
-import {$on, qsa, removeClass} from './helpers'
-import Controller from './controller'
-import Model from './model'
-import Store from './store'
-import Template from './template'
+  /**
+   * Sets up a brand new Todo list.
+   *
+   * @param {string} name The name of your new to do list.
+   */
+  function Todo(name) {
+    this.storage = new app.Store(name);
+    this.model = new app.Model(this.storage);
+    this.template = new app.Template();
+    this.view = new app.View(this.template);
+    this.controller = new app.Controller(this.model, this.view);
+  }
 
-var todo
+  var todo = new Todo('todos-vanillajs');
 
-/**
- * Sets up a brand new Todo list.
- *
- * @param {string} name The name of your new to do list.
- */
-function Todo(name) {
-  this.storage = new Store(name)
-  this.model = new Model(this.storage)
-  this.template = new Template()
-  this.view = new View(this.template)
-  this.controller = new Controller(this.model, this.view)
-}
+  function setView() {
+    todo.controller.setView(document.location.hash);
+  }
 
-function setView() {
-  todo.controller.setView(document.location.hash)
-}
-
-$on(window, 'load', function() {
-  todo = new Todo('todos-vanillajs')
-  setView()
-  removeClass(qsa('.hide'), 'hide')
-})
-$on(window, 'hashchange', setView)
+  $on(window, 'load', setView);
+  $on(window, 'hashchange', setView);
+})();
